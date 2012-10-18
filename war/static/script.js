698c93f22cb8b3222982ce70c28ecb1035e58834
Thomas Taschauer <tomtasche@gmail.com>
2012-10-18 19:47:03 +0200

    dump

--- war/static/script.js
+++ war/static/script.js
@@ -1,24 +1,11 @@
-/*
- * Copyright (c) 2012 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 var onSuccess = function(data, result, xhr) {
   if (data.redirect) {
     window.location.href = data.redirect;
   } else {
-    document.getElementById("heading").innerText = data['title'];
+//    document.getElementById("heading").innerText = data['title'];
     document.title += ' - ' + data['title'];
-    document.getElementById('content').innerHTML = data['content'];
+//    document.getElementById('content').innerHTML = data['content'];
+    document.body.innerHTML = data['content'];
   }
 };
 
